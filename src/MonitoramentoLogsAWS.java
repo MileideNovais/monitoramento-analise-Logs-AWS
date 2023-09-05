1dce1e523c771b42cb7c09930b81e8fd2e732947
Mileide Novais <mileide.financeiro@gmail.com>
2023-09-05 00:24:00 -0300

    Initial commit

---
+++ src/MonitoramentoLogsAWS.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MonitoramentoLogsAWS {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int quantidadeLogs = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer do scanner
+
+        Map<String, Long> eventosPorServico = new HashMap<>();
+        for (int i = 0; i < quantidadeLogs; i++) {
+            String[] parts = scanner.nextLine().split(",");
+            String servico = parts[1];
+
+            //TODO: Utilize o mapa para registrar/incrementar o serviço em questão.
+            eventosPorServico.put(servico, eventosPorServico.getOrDefault(servico, 0L) + 1);
+        }
+
+        //TODO: Identifique no mapa os serviços com maior e menor quantidade de logs.
+        //   Dica: Utilize Java Streams para tornar essa tarefa mais simples.
+        Map.Entry<String, Long> maiorServico = eventosPorServico.entrySet().stream()
+                .max(Map.Entry.comparingByValue())
+                .orElseThrow(NoSuchElementException::new);
+
+        Map.Entry<String, Long> menorServico = eventosPorServico.entrySet().stream()
+                .min(Map.Entry.comparingByValue())
+                .orElseThrow(NoSuchElementException::new);
+
+        //TODO: Imprima a saída no padrão definido no enunciado deste desafio.
+        System.out.println("Eventos por servico:");
+        eventosPorServico.forEach((nomeServico, quantidade) -> {
+            System.out.println(nomeServico + ":" + quantidade);
+        });
+
+        System.out.println("Maior:" + maiorServico.getKey());
+        System.out.println("Menor:" + menorServico.getKey());
+
+        scanner.close();
+    }
+}
